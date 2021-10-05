86a085523951502c4eea397330704b9a7860593e
martinagorriaran <martina.gorriaraan@gmail.com>
2021-10-05 14:08:48 -0300

    firebase

--- firebase-ej1/main.js
+++ firebase-ej1/main.js
@@ -17,4 +17,74 @@ function guardarCliente(){
 
     db.collection("cliente").doc().set(unCliente)
     vaciar()
+    listarClientes()
+}
+
+
+async function listarClientes(){
+   const clientes= await db.collection("cliente").get()
+
+   let lista_cliente = [];
+
+   const lista_conformada= clientes.docs.map( doc =>{
+
+    lista_clientes = doc.data()   
+    lista_clientes.id = doc.id;
+    
+    return lista_clientes
+   });
+
+   let lista_tr = []
+
+   //vamos a generar dinamica la tabla
+   lista_conformada.forEach(element => {
+    let tr = `
+        <tr>
+            <td>${element.nombre}</td>
+            <td>${element.apellido}</td>
+            <td>${element.dni}</td>
+            <td>
+                <button onclick="eliminarCliente('${element.id}')" class="btn btn-danger btn-sm">x</button>
+                <button onclick="editarCliente('${element.nombre}','${element.apellido}',${element.dni})" class="btn btn-primary btn-sm">Edit</button>
+
+            </td>
+        </tr>
+    `
+    lista_tr.push(tr)
+   });
+
+   document.getElementById("tbody").innerHTML = lista_tr.join('')
+   
+}
+
+listarClientes()
+
+function eliminarCliente(id){
+db.collection("cliente").doc(id).delete();
+listarClientes()
+
+}
+
+function editarCliente(n,a,d,i){
+    document.getElementById("inp_nom").value=n;
+    document.getElementById("inp_ape").value=a;
+    document.getElementById("inp_dni").value=d;
+    document.getElementById("inp_id").value=i;
+
+    console.log(unCliente)
+
+}
+
+function actualizarCliente(){
+    const clienteActualizado = {
+        nombre: document.getElementById("inp_nom").value,
+        apellido: document.getElementById("inp_ape").value,
+        dni: document.getElementById("inp_dni").value
+    }
+
+    let id = document.getElementById("inp_id").value
+
+    db.collection("cliente").doc(id).update(clienteActualizado)
+
+    listarClientes()
 }
